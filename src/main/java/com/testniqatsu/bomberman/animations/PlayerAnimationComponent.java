923734f95daf9d0045a543fa832b196288a223ef
TruongKhoiNguyen <truongkhoi31052002@gmail.com>
2021-11-26 17:48:17 +0700

    0.10.2 minor changes

--- src/main/java/com/testniqatsu/bomberman/animations/PlayerAnimationComponent.java
+++ src/main/java/com/testniqatsu/bomberman/animations/PlayerAnimationComponent.java
@@ -14,6 +14,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerAnimationComponent extends Component {
+    final String spriteSheetName = "wizard.png";
+    final int spriteSheetWidth = 96;
+    final int spriteSheetHeight = 128;
+
+    final int columns = 3;
+    final int rows = 4;
+
+    final Image spriteSheetImage = FXGL.image(spriteSheetName, spriteSheetWidth, spriteSheetHeight);
+    final int framePerRows = columns;
+    final int spriteWidth = spriteSheetWidth / columns;
+    final int spriteHeight = spriteSheetHeight / rows;
+    final Duration duration = Duration.seconds(1);
 
     enum State {
         STAY_DOWN,
@@ -30,15 +42,17 @@ public class PlayerAnimationComponent extends Component {
     AnimatedTexture texture;
     Point2D centerPoint;
 
-    Image spriteSheetImage;
-    int framePerRows;
-    int spriteWidth;
-    int spriteHeight;
-    Duration duration;
-
-    String spriteSheetName;
-    int spriteSheetWidth;
-    int SpriteSheetHeight;
-
     public PlayerAnimationComponent() {}
+
+    AnimationChannel loadAnimation(int startFrame, int endFrame) {
+        return new AnimationChannel(
+                spriteSheetImage,
+                framePerRows,
+                spriteWidth,
+                spriteHeight,
+                duration,
+                startFrame,
+                endFrame
+        );
+    }
 }
