c76d6e07bf547c4ab876c47e64ad21c2eb47cb5a
TruongKhoiNguyen <truongkhoi31052002@gmail.com>
2021-12-02 21:49:59 +0700

    0.10.17 change some constants

--- src/main/java/com/testniqatsu/bomberman/BombermanApp.java
+++ src/main/java/com/testniqatsu/bomberman/BombermanApp.java
@@ -19,6 +19,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.util.Duration;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Map;
@@ -31,8 +32,10 @@ public class BombermanApp extends GameApplication {
     private static final int VIEW_WIDTH = 720;
     private static final int VIEW_HEIGHT = 720;
 
-    public static final String TITLE = "BOMBERMAN";
-    public static final String VERSION = "1.0";
+    private static final String TITLE = "BOMBERMAN";
+    private static final String VERSION = "1.0";
+
+    private static final String FONT = "Quinquefive-Ea6d4.ttf";
 
     public static boolean sound_enabled = true;
     private boolean requestNewGame = false;
@@ -48,13 +51,15 @@ public class BombermanApp extends GameApplication {
         settings.setGameMenuEnabled(true);
         settings.setPreserveResizeRatio(true);
         settings.setManualResizeEnabled(true);
-        settings.setFontUI("Quinquefive-Ea6d4.ttf");
+        settings.setFontUI(FONT);
         settings.setSceneFactory(new SceneFactory() {
+            @NotNull
             @Override
             public FXGLMenu newMainMenu() {
                 return new BombermanMenu();
             }
 
+            @NotNull
             @Override
             public FXGLMenu newGameMenu() {
                 return new BombermanGameMenu();
@@ -225,10 +230,6 @@ public class BombermanApp extends GameApplication {
         onCollisionBegin(BombermanType.PLAYER, BombermanType.FIRE, (p, f) -> onPlayerKilled());
 
         onCollisionBegin(BombermanType.PLAYER, BombermanType.BALLOOM_E, (p, b) -> onPlayerKilled());
-    }
-
-    private void gameOver() {
-        getDialogService().showMessageBox("Demo Over. Press OK to exit", getGameController()::exit);
     }
 
     private void onPlayerKilled() {
