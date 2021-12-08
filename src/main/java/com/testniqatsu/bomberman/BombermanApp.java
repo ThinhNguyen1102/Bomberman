3255d05c55dd3ee662637ba7e80a3b62ef36539a
Hoàng Quý <78021023+quysterben@users.noreply.github.com>
2021-12-09 00:30:43 +0700

    Config Sprites.png

--- src/main/java/com/testniqatsu/bomberman/BombermanApp.java
+++ src/main/java/com/testniqatsu/bomberman/BombermanApp.java
@@ -275,8 +275,7 @@ public class BombermanApp extends GameApplication {
     }
 
     private void setLevel() {
-        //FXGL.setLevelFromMap("bbm_level" + FXGL.geti("level") + ".tmx");
-        FXGL.setLevelFromMap("bbm_level2.tmx");
+        FXGL.setLevelFromMap("bbm_level" + FXGL.geti("level") + ".tmx");
         Viewport viewport = getGameScene().getViewport();
         viewport.setBounds(0, 0, GAME_WORLD_WIDTH, GAME_WORLD_HEIGHT);
         viewport.bindToEntity(
